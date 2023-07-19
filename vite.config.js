681eb4012a78a62c35fcb726e81c54bce5e4aadc
Rahul Tiwari <tiwarirahul0809@gmail.com>
2023-07-19 14:43:34 +0530

    uibuild

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // string shorthand: http://localhost:5173/foo -> http://localhost:4567/foo
+      '/api/users/signup': 'http://localhost:3000',
+      "/api/users/purchasedCourses": 'http://localhost:3000',
+      "/api/users/login": 'http://localhost:3000',
+      "/api/users/courses": 'http://localhost:3000',
+      "/api/users/courses/:id": 'http://localhost:3000'
+    }
+  }
 })
